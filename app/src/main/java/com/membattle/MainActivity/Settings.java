5229151dc51a0e7f296f7c7cda06547ff33e8e14
MrSwimmer <mrswimmerlab@gmail.com>
2017-12-30 18:57:11 +0700

    уведомления firebase

--- app/src/main/java/com/membattle/MainActivity/Settings.java
+++ app/src/main/java/com/membattle/MainActivity/Settings.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.appinvite.AppInviteInvitation;
 import com.membattle.SignInUp.LoginActivity;
 import com.membattle.R;
 import com.membattle.WidgetPlus.TextViewPlus;
